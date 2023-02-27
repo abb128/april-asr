6ea959d195c05fbc2081206764eb95dc22ff51b8
abb128 <65567823+abb128@users.noreply.github.com>
2023-02-26 21:24:47 -0600

    Fix cURL command

--- download_onnx_windows_x64.sh
+++ download_onnx_windows_x64.sh
@@ -5,7 +5,7 @@ mkdir lib
 
 set -e
 cd lib
-curl -O https://github.com/microsoft/onnxruntime/releases/download/v1.13.1/onnxruntime-win-x64-1.13.1.zip
+curl -LO https://github.com/microsoft/onnxruntime/releases/download/v1.13.1/onnxruntime-win-x64-1.13.1.zip
 unzip onnxruntime-win-x64-1.13.1.zip
 mv onnxruntime-win-x64-1.13.1/* .
 rm -r onnxruntime-win-x64-1.13.1
