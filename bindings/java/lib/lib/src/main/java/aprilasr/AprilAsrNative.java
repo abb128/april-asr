aecda896ed3f4ffa1843b5668ccf7b48741aef77
abb128 <65567823+abb128@users.noreply.github.com>
2023-03-06 17:55:57 -0600

    Java: add initial native API definitions

---
+++ bindings/java/lib/lib/src/main/java/aprilasr/AprilAsrNative.java
@@ -0,0 +1,90 @@
+package aprilasr;
+
+import com.sun.jna.Native;
+import com.sun.jna.Platform;
+import com.sun.jna.Pointer;
+import com.sun.jna.NativeLong;
+import com.sun.jna.Callback;
+import com.sun.jna.Structure;
+import com.sun.jna.Structure.FieldOrder;
+import java.io.File;
+import java.io.InputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+
+public class AprilAsrNative {
+    @FieldOrder({"data"})
+    public static class AprilSpeakerID extends Structure {
+        byte[] data = new byte[16];
+    };
+
+    @FieldOrder({"token","logprob","flags"})
+    public static class AprilToken extends Structure {
+        String token;
+        float logprob;
+        int flags;
+    };
+
+    public static interface AprilRecognitionResultHandler extends Callback {
+        void invoke(Pointer userdata, int result, NativeLong count, AprilToken[] tokens);
+    }
+
+    @FieldOrder({"speaker", "handler", "userdata", "flags"})
+    public static class AprilConfig extends Structure {
+        AprilSpeakerID speaker;
+        
+        AprilRecognitionResultHandler handler;
+        Pointer userdata;
+
+        int flags;
+    };
+
+    private static void unpackDll(File targetDir, String lib) throws IOException {
+        InputStream source = AprilAsrNative.class.getResourceAsStream("/win32-x86-64/" + lib + ".dll");
+        Files.copy(source, new File(targetDir, lib + ".dll").toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    static {
+        if (Platform.isWindows()) {
+            // We have to unpack dependencies
+            try {
+                // To get a tmp folder we unpack small library and mark it for deletion
+                File tmpFile = Native.extractFromResourcePath("/win32-x86-64/empty", AprilAsrNative.class.getClassLoader());
+                File tmpDir = tmpFile.getParentFile();
+                new File(tmpDir, tmpFile.getName() + ".x").createNewFile();
+
+                // Now unpack dependencies
+                unpackDll(tmpDir, "onnxruntime");
+            } catch (IOException e) {
+                // Nothing for now, it will fail on next step
+            } finally {
+                Native.register(AprilAsrNative.class, "libaprilasr");
+            }
+        } else {
+            Native.register(AprilAsrNative.class, "aprilasr");
+        }
+    }
+
+    public static final int APRIL_VERSION = 1;
+
+    public static native void aam_api_init(int version);
+    public static native Pointer aam_create_model(String path);
+
+    public static native String aam_get_name(Pointer model);
+    public static native String aam_get_description(Pointer model);
+    public static native String aam_get_language(Pointer model);
+
+    public static native NativeLong aam_get_sample_rate(Pointer model);
+
+    public static native void aam_free(Pointer model);
+
+    public static native Pointer aas_create_session(Pointer model, AprilConfig config);
+    public static native void aas_feed_pcm16(Pointer session, short[] pcm16, NativeLong short_count);
+    public static native void aas_flush(Pointer session);
+
+    public static native float aas_realtime_get_speedup(Pointer session);
+
+    public static native void aas_free(Pointer session);
+}
