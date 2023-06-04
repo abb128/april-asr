51eef65e543a215837aa63ee84b9a6059045481c
Denzell Ford <fdenzell@gmail.com>
2023-06-03 23:26:12 -0500

    Rust: linked  april_api.h to exist in rust code, for releases

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -20,7 +20,7 @@ fn main() {
     let bindings = bindgen::Builder::default()
         // The input header we would like to generate
         // bindings for.
-        .header("../../april_api.h")
+        .header("april_api.h")
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
